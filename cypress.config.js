91d935697381b9f7e82139399aceaad52aeebb51
Lisandro Alfredo Suarez Salinas <lisan3@hotmail.com>
2026-05-19 01:07:55 -0600

    Configuracion de proyecto base

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
+  
   e2e: {
+    baseUrl: "https://www.saucedemo.com",
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    video: true,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
 });
